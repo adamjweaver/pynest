7039929ae1d4607e2773f01816455ec4f3f9a1ee
Adam Weaver <adamjweaver@gmail.com>
2018-02-15 22:28:58 +0000

    rewrite submission script to parse correct temps and send to google form. also combine in to one request to nest api

--- submit.sh
+++ submit.sh
@@ -1,4 +1,31 @@
-current=`nest.py --user $1 --password $2 --celsius curtemp`
-target=`nest.py --user $1 --password $2 --celsius tartemp`
+allData=`nest.py --user $1 --password $2 show`
+eco=`echo "$allData" | grep "eco\."`
 
-curl -X POST -H "Content-Type: application/json" -d '{"value1":"'${current}'","value2":"'${target}'"}' https://maker.ifttt.com/trigger/nest_temp/with/key/$3
+targetTemp=`echo "$allData" | grep "target_temperature\." | egrep -o "[0-9]+\.[0-9]?"`
+currentTemp=`echo "$allData" | grep "current_temperature\." | egrep -o "[0-9]+\.[0-9]?"`
+heatingOn=`echo "$allData" | grep  "hvac_heater_state[\.]*:.*" | egrep -o "\s.*" | egrep -o "[A-Za-z]+"`
+
+# if [[ $eco = *"schedule"* ]]; then
+	
+# el
+if [[ $eco = *"auto-eco"* ]]; then
+  targetTemp=`echo "$allData" | grep "away_temperature_low\." | egrep -o "[0-9]+\.[0-9]?"`
+elif [[ $eco = *"manual-eco"* ]]; then
+  targetTemp=`echo "$allData" | grep "away_temperature_low\." | egrep -o "[0-9]+\.[0-9]?"`
+fi
+
+currentField="entry.1120619444"
+targetField="entry.23449027"
+onField="entry.333433863"
+
+curl https://docs.google.com/forms/d/$3/formResponse -d ifq -d $currentField=$currentTemp -d $targetField=$targetTemp -d $onField=$heatingOn -d submit=Submit
+
+echo "target $targetTemp"
+echo "heatingOn $heatingOn"
+echo "currentTemp $currentTemp"
+# current=`nest.py --user $1 --password $2 --celsius curtemp`
+# target=`nest.py --user $1 --password $2 --celsius tartemp`
+# eco=`nest.py --user $1 --password $2 --celsius eco`
+
+# curl -X POST -H "Content-Type: application/json" -d '{"value1":"'${current}'","value2":"'${target}'"}' https://maker.ifttt.com/trigger/nest_temp/with/key/$3
+
